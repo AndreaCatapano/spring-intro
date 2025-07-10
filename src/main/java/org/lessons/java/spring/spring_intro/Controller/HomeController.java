2b8de2245826239e1ec57c8ddf33d26eb7da69a7
AndreaCatapano <a.catapano96@gmail.com>
2025-07-10 16:30:59 +0200

    feat: added new page for testing controller

--- src/main/java/org/lessons/java/spring/spring_intro/Controller/HomeController.java
+++ src/main/java/org/lessons/java/spring/spring_intro/Controller/HomeController.java
@@ -1,8 +1,13 @@
 package org.lessons.java.spring.spring_intro.Controller;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -11,6 +16,27 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/greetings")
+    public String getMethodName(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greetings";
+    }
+
+    @GetMapping("/user")
+    public String getMethodName(@RequestParam(name = "name") String name,
+            @RequestParam(name = "surname") String surname,
+            Model model) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String currentTime = LocalTime.now().format(formatter);
+
+        model.addAttribute("name", name);
+        model.addAttribute("surname", surname);
+        model.addAttribute("date", currentTime);
+
+        return "user";
     }
 
 }
